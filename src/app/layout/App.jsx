5f767c82b13e8af7dd6bf542ea4944a73100fd26
Raja <kingrenu13@gmail.com>
2020-07-31 02:12:48 +0800

    End of Section 4 Form

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -4,13 +4,31 @@ import NavBar from '../../features/nav/NavBar';
 import { Container } from 'semantic-ui-react';
 
 export default function App() {
-
+  //hooks
   const [formOpen, setFormOpen] = useState(false);
+
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
+
   return (
     <Fragment>
-      <NavBar setFormOpen={setFormOpen} />
+      <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className="main">
-        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+        <EventDashboard
+          formOpen={formOpen}
+          setFormOpen={setFormOpen}
+          selectEvent={handleSelectEvent}
+          selectedEvent={selectedEvent}
+        />
       </Container>
     </Fragment>
   );
--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -4,18 +4,41 @@ import EventList from './EventList';
 import EventForm from '../eventForm/EventForm';
 import {sampleData} from '../../../app/api/sampleData';
 
-export default function EventDashboard({formOpen, setFormOpen}) {
+export default function EventDashboard({formOpen, setFormOpen, selectEvent, selectedEvent}) {
     const [events, setEvents] = useState(sampleData);
 
+
+    function handleCreateEvent(event) {
+        setEvents([...events, event])
+    }
+
+    function handleUpdateEvent(updatedEvent) {
+        setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+        selectEvent(null);
+    }
+  
+    function handleDeleteEvent(eventId) {
+        setEvents(events.filter(evt => evt.id !== eventId));
+    }
+  
+    
     return (
-        <Grid>
-            <Grid.Column width={10}>
-                <EventList events={events} />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                {formOpen &&
-                <EventForm setFormOpen={setFormOpen}/>}
-            </Grid.Column>
-        </Grid>
-    )
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events} selectEvent={selectEvent} deleteEvent={handleDeleteEvent} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
+            updateEvent={handleUpdateEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
 }
